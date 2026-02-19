04670d1e660f4a25f16f6b6f8ec380c1aca9efe3
Ridwan Adhi Pratama <ridwanadhip@outlook.co.id>
2026-02-20 02:23:14 +0700

    1. implement upcoming and bookable api 2. change get-events-by-venue path

--- src/main/java/id/my/ridwanadhip/ticketbooking/event/EventController.java
+++ src/main/java/id/my/ridwanadhip/ticketbooking/event/EventController.java
@@ -2,20 +2,23 @@ package id.my.ridwanadhip.ticketbooking.event;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping(path="/api/v1/event")
 public class EventController {
+    private static final int DEFAULT_UPCOMING_EVENT_DAY_LIMIT = 7;
+
     private final EventRepository eventRepository;
 
     public EventController(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
     }
 
-    @GetMapping(path="/{venueId}")
+    @GetMapping(path="/venue/{venueId}")
     public List<Event> findAllByVenueId(
             @PathVariable long venueId,
             @RequestParam(name="page", defaultValue = "0") int page,
@@ -24,5 +27,32 @@ public class EventController {
 
         Pageable paging = PageRequest.of(page, pageSize);
         return eventRepository.findAllByVenueId(venueId, paging).getContent();
+    }
+
+    @GetMapping(path="/upcoming")
+    public List<Event> findUpcomingEvents(
+            @RequestParam(name="until", required = false) LocalDateTime until,
+            @RequestParam(name="page", defaultValue = "0") int page,
+            @RequestParam(name="pageSize", defaultValue = "10") int pageSize
+    ) {
+
+        var since = LocalDateTime.now();
+        if (until == null) {
+            until = since.plusDays(DEFAULT_UPCOMING_EVENT_DAY_LIMIT);
+        }
+
+        Pageable paging = PageRequest.of(page, pageSize, Sort.by("startAt"));
+        return eventRepository.findAllByStartAtBetween(since, until, paging).getContent();
+    }
+
+    @GetMapping(path="/bookable")
+    public List<Event> findBookableEvents(
+            @RequestParam(name="page", defaultValue = "0") int page,
+            @RequestParam(name="pageSize", defaultValue = "10") int pageSize
+    ) {
+
+        var now = LocalDateTime.now();
+        Pageable paging = PageRequest.of(page, pageSize);
+        return eventRepository.findAllByBetweenBookingTime(now, paging).getContent();
     }
 }
--- src/main/java/id/my/ridwanadhip/ticketbooking/event/EventRepository.java
+++ src/main/java/id/my/ridwanadhip/ticketbooking/event/EventRepository.java
@@ -3,10 +3,15 @@ package id.my.ridwanadhip.ticketbooking.event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+import java.time.LocalDateTime;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findAllByVenueId(Long venueId, Pageable pageable);
+    Page<Event> findAllByStartAtBetween(LocalDateTime since, LocalDateTime until, Pageable pageable);
+
+    @NativeQuery("SELECT * FROM event e WHERE ?1 BETWEEN e.booking_start_at and e.booking_finish_at")
+    Page<Event> findAllByBetweenBookingTime(LocalDateTime now, Pageable pageable);
 }
